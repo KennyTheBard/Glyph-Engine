6efcb097ef56dd7ca712f79effae67363f332516
KennyTheBard <octav_teculescu@yahoo.com>
2019-09-05 22:20:17 +0300

    Made a small server with Gin Gonic

--- server.go
+++ server.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	router = gin.Default()
+	router.GET("/api", handleGet)
+	router.Run()
+}
+
+func handleGet(context *gin.Context) {
+	msg, _ := context.GetQuery("name")
+	context.String(http.StatusOK, "Your name is "+msg)
+}
