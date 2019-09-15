3beb0856f04a22f1d321068b8ff48f289612f9fb
KennyTheBard <octav_teculescu@yahoo.com>
2019-09-15 15:01:57 +0300

    Got golint off my butt for exported fields

--- data/init_db.go
+++ data/init_db.go
@@ -2,13 +2,16 @@ package data
 
 import (
 	"github.com/jinzhu/gorm"
+	// this is need in order to be able to use postgres syntax
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 
 	model "../model"
 )
 
+// DB is the reference to the ORM that comunicates with th data source
 var DB *gorm.DB
 
+// Init prepares the link with the data source
 func Init() {
 	var err error
 	DB, err = gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=moonshine_square password=postgres sslmode=disable")
