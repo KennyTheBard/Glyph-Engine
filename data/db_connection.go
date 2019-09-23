2afdaf31e6a5b4970d9a2188e8c3817c5a66805d
KennyTheBard <octav_teculescu@yahoo.com>
2019-09-23 20:46:27 +0300

    Cleaned db migrations a bit

--- data/db_connection.go
+++ data/db_connection.go
@@ -19,21 +19,19 @@ func Init(cleanStart bool) {
 		panic("failed to connect database")
 	}
 
-	if cleanStart {
-		DB.DropTableIfExists(&model.StoryModel{})
-		DB.DropTableIfExists(&model.ChoiceModel{})
-		DB.DropTableIfExists(&model.ItemModel{})
-		DB.DropTableIfExists(&model.ItemCost{})
-		DB.DropTableIfExists(&model.ItemReward{})
-		DB.DropTableIfExists(&model.ItemRequirement{})
-	}
+	migrate(&model.StoryModel{}, cleanStart)
+	migrate(&model.ChoiceModel{}, cleanStart)
+	migrate(&model.ItemModel{}, cleanStart)
+	migrate(&model.ItemCost{}, cleanStart)
+	migrate(&model.ItemReward{}, cleanStart)
+	migrate(&model.ItemRequirement{}, cleanStart)
+}
 
-	DB.AutoMigrate(&model.StoryModel{})
-	DB.AutoMigrate(&model.ChoiceModel{})
-	DB.AutoMigrate(&model.ItemModel{})
-	DB.AutoMigrate(&model.ItemCost{})
-	DB.AutoMigrate(&model.ItemReward{})
-	DB.AutoMigrate(&model.ItemRequirement{})
+func migrate(dbmodel interface{}, cleanStart bool) {
+	if cleanStart {
+		DB.DropTableIfExists(&dbmodel)
+	}
+	DB.AutoMigrate(&dbmodel)
 }
 
 func Close() {
