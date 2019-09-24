08e441f5dd5833077f7b4c3ce2288b9d9d0f26db
KennyTheBard <octav_teculescu@yahoo.com>
2019-09-24 18:23:59 +0300

    Added export function

--- port/export.go
+++ port/export.go
@@ -1,1 +1,31 @@
 package port
+
+import (
+	"bufio"
+	"os"
+)
+
+func Export(fileName string, objects interface{})) error {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	for object := range objects {
+		bs, err := json.Marshal(object)
+		if err != nil {
+			return err
+		}
+
+		_, err := writer.Write(bs)
+		if err != nil {
+			return err
+		}
+	}
+
+	writer.Flush()
+	return nil
+}
