5a35d799d91ab7dddd2e8d2022cd36301a82cab2
KennyTheBard <octav_teculescu@yahoo.com>
2019-09-25 22:04:00 +0300

    Added energy cost to choice model

--- model/choice.go
+++ model/choice.go
@@ -5,6 +5,7 @@ type ChoiceModel struct {
 	ID            uint        `json:"id" 				gorm:"primary_key"`
 	Name          string      `json:"name"`
 	Text          string      `json:"text"`
+	EnergyCost    uint        `json:energyCost`
 	ParentStoryID uint        `json:"parentStoryID" 	gorm:"column:parent_story_id"`
 	NextStoryID   uint        `json:"nextStoryID" 		gorm:"column:next_story_id"`
 	NextStory     StoryModel  `json:"nextStory" 		gorm:"foreignkey:ID"`
@@ -15,6 +16,7 @@ func (choice ChoiceModel) ToDto() (ret struct {
 	ID            uint        `json:"id"`
 	Name          string      `json:"name"`
 	Text          string      `json:"text"`
+	EnergyCost    uint        `json:energyCost`
 	ParentStoryID uint        `json:"parentStoryID"`
 	NextStoryID   uint        `json:"nextStoryID"`
 	Stacks        []ItemStack `json:"stacks"`
@@ -22,6 +24,7 @@ func (choice ChoiceModel) ToDto() (ret struct {
 	ret.ID = choice.ID
 	ret.Name = choice.Name
 	ret.Text = choice.Text
+	ret.EnergyCost = choice.EnergyCost
 	ret.ParentStoryID = choice.ParentStoryID
 	ret.NextStoryID = choice.NextStoryID
 	ret.Stacks = choice.Stacks
